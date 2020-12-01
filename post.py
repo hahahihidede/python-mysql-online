acc1261747d973e8a6482921fd0dc5c31b6b3a41
Dede Rohmat H <54473030+hahahihidede@users.noreply.github.com>
2020-12-01 10:33:45 +0700

    Update post.py

--- post.py
+++ post.py
@@ -1,6 +1,6 @@
 import requests
 api_key_value = "9F7j3bA5TAmPt"
-url = "http://inoprex.com/projectbti/API/API.php
+url = "http://www.yourwebwite.com/api.php
 data = "api_key=" + api_key_value + "&jenis_sampah=" +"organik"+""
 headers = {"Content-Type": "application/x-www-form-urlencoded"}
 requests.post(url,data=data,headers=headers)
